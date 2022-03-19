6d241300dec9e97957f6370dc4e5d82db438a9d0
Γιώργος Σταθόπουλος <72932804+Giorgosstath@users.noreply.github.com>
2022-03-19 15:27:41 +0200

    Delete Data Structures directory

--- Data Structures/project2/src/DiskComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class DiskComparator implements Comparator
-{
-    public int compare(Object t1, Object t2)
-	{
-        return ((Disk) t1).getFreeSpace() - ((Disk) t2).getFreeSpace();
-    }
-}
--- Data Structures/project2/src/EmptyListException.java
+++
@@ -1,6 +0,0 @@
-/**
- * EmptyListException is a simple Exception.
- * It is thrown by a List when get methods are called on an empty list.
- */
-public class EmptyListException extends Exception {
-}
